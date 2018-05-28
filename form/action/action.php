83a572f8a8e9e1e1c72e4bb2d4807e41300dd269
KIIT <KIIT@BT1701083>
2018-05-28 18:06:16 +0530

    first commit

---
+++ form/action/action.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+
+<div class="container-fluid">
+   <div class="row">
+   		<div class="col-md-12">
+   			<?php
+                if(isset($_POST['submit']))
+                {
+                    ?>
+                    
+                    <div class="alert alert-success text-center">
+                      <strong>Success!</strong><?php echo "First Name: ".$first_name = $_POST['first_name']."<br>"; ?>
+                    </div>
+                    <div class="alert alert-success text-center">
+                      <strong>Success!</strong><?php echo "Last Name: ".$last_name = $_POST['last_name']."<br>";?>
+                    </div>
+                    <?php 
+                }
+           ?>
+   		</div>
+   </div>
+</div>
+
+</body>
+</html>
---
+++ form/html_form_post.php
@@ -0,0 +1,39 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Bootstrap Example</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+</head>
+<body>
+<br/><br/>
+<div class="container">
+  <div class="row">
+  	<div class="col-md-12 well text-center">
+  		<h1>Input Form</h1>
+  	</div>
+  </div>
+  <div class="row">
+  	<div class="col-md-2"></div>
+  	        <div class="col-md-8 well">
+          		<form action="action/action.php" method="post">
+                  <div class="form-group">
+                    <label>First Name:</label>
+                    <input type="text" name="first_name" class="form-control">
+                  </div>
+                  <div class="form-group">
+                    <label for="last_name">Last Name:</label>
+                    <input type="text" name="last_name" class="form-control">
+                  </div>
+                  <button type="submit" name="submit" class="btn btn-danger">Submit</button>
+                </form> <!-- Form End -->
+          	</div><!-- Middle Col end -->
+          	<div class="col-md-2"></div><!-- third col end -->
+          </div> <!-- Row End -->
+     </div><!-- Container End -->
+
+</body>
+</html>
